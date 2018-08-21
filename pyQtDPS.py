b3935d3cad42f71b8f520fe6332cc73bea87aeb0
Kellerbase <base@keller.de>
2018-08-21 03:56:32 +0200

    Plot added

--- pyQtDPS.py
+++ pyQtDPS.py
@@ -16,8 +16,11 @@ try:
 	import minimalmodbus
 except ImportError:
 	sys.exit("\nERROR\nminimalmodbus for Python3 not found!\nPlease install with 'pip3 install minimalmodbus'")
-
-
+try:
+    import pyqtgraph as pg
+    plotEnabled = True
+except ImportError:
+    plotEnabled = False
 
 stopMe = False
 
@@ -29,19 +32,34 @@ class Updater(QtCore.QThread):
         while not stopMe:
             self.sleep(config["update_rate"])
             self.received.emit()
+
+class UpdaterFast(QtCore.QThread):
+    received = QtCore.pyqtSignal()
+
+    def run(self):
+        config = load_config()
+        while not stopMe:
+            self.sleep(config["plot_update_rate"])
+            self.received.emit()
+            
             
 class PyQtDPS(QtWidgets.QMainWindow):
     def __init__(self):
         super(PyQtDPS, self).__init__()
+        if not plotEnabled:
+            self.plotButton.hide()
         uic.loadUi("dpsGUI.ui", self)
         self.config = load_config()
-		
+        self.ampSpinBox.setMaximum(self.config["maxAmps"])
         if not os.path.exists(config["dev_path"]):
             print("Initializing USB or Bluetooth communication")
             try:
+                self.processRunning = True
                 self.process = subprocess.Popen(self.config["dev_init_command"], stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
             except:
                 sys.exit("\nERROR\nSystem command from config.json exited with an error. \nPlease modify 'dev_init_command' in config.json")
+        else:
+            self.processRunning = False
         print("Setting permissions to access device")
         ok = os.system("sudo chmod +rwx "+config["dev_path"])
         if ok != 0:
@@ -56,12 +74,11 @@ class PyQtDPS(QtWidgets.QMainWindow):
             self.closeWindow()
             sys.exit("\nERROR\nFailed to initialize minimalmodbus instrument!\nPlease check serial-settings in config.json")
         print("Starting thread to update GUI-values")
-        self.updater = Updater(self)
-        self.updater.received.connect(self.refresh)
-        self.updater.start()
         
         self.setButton.clicked.connect(self.setValues)
         self.powerButton.clicked.connect(self.togglePower)
+        self.lockedButton.clicked.connect(self.toggleLocked)
+        self.plotButton.clicked.connect(self.startPlotThread)
         ok = False
         while not ok:
             try:
@@ -72,7 +89,15 @@ class PyQtDPS(QtWidgets.QMainWindow):
                 time.sleep(1)
         self.voltageSpinBox.setValue(a[0]/100) # U-set x100 (R/W)
         self.ampSpinBox.setValue(a[1]/100) # I-set x100 (R/W)
+        self.setDisplay(a)
         self.show()
+        
+        #self.updater = Updater(self)
+        #self.updater.received.connect(self.refresh)
+        self.updater = QtCore.QTimer()
+        self.updater.setInterval(self.config["update_rate"])
+        self.updater.timeout.connect(self.refresh)
+        self.updater.start()
         
     def togglePower(self):
         try :
@@ -80,18 +105,34 @@ class PyQtDPS(QtWidgets.QMainWindow):
             onoff=self.power_supply.read_register(9)
             # self.powerButton.setChecked(bool(onoff))
             self.power_supply.write_register(9,(1-onoff))
+            if onoff==0:
+                self.lockedButton.setText("Locked")
+            else:
+                self.lockedButton.setText("Unlocked")
         except :
             print ("\nERROR\nChanging power-state failed!\n")
+            
+    def toggleLocked(self):
+        try :
+            print("Changing locked-state")
+            onoff=self.power_supply.read_register(6)
+            # self.powerButton.setChecked(bool(onoff))
+            self.power_supply.write_register(6,(1-onoff))
+        except :
+            print ("\nERROR\nChanging locked-state failed!\n")
             
     def setValues(self):
         try :
             print("Setting output values")
             volt = self.voltageSpinBox.value()
             amp = self.ampSpinBox.value()
-            self.power_supply.write_register(0,volt*100)
-            self.power_supply.write_register(1,amp*100)
+            self.power_supply.write_register(0,int(volt*100))
+            self.power_supply.write_register(1,int(amp*100))
         except :
             print ("\nERROR\nSetting output values failed!\n")
+            
+    def startPlotThread(self):
+        self.initPlotWindow()
             
     def refresh(self):
         try :
@@ -103,41 +144,127 @@ class PyQtDPS(QtWidgets.QMainWindow):
             #a[4] P-out x100
             #a[5] U-in x100
             #a[6] lock/unlock 1/0 (R/W)
-            #a[7] ?
+            #a[7] Protected 1/0
             #a[8] operating mode CC/CV 1/0
             #a[9] on/off 1/0 (R/W)
             #a[10] display intensity 1..5 (R/W)
-            self.voltageLCD.display(a[2]/100)
-            self.ampLCD.display(a[3]/100)
-            self.powerLCD.display(a[4]/100)
-            self.vInLabel.setText(str(a[5]/100)+" V (V-In)")
-            self.voltageSpinBox.setMaximum(a[5]/100)
-            if(a[9]==1):
-                self.powerButton.setStyleSheet("background-color: green")
-            else:
-                self.powerButton.setStyleSheet("background-color: red")
-            if a[0]==self.voltageSpinBox.value()*100:
-                self.voltageSpinBox.setStyleSheet("background-color: green")
-            else:
-                self.voltageSpinBox.setStyleSheet("background-color: red")
-            if a[1]==self.ampSpinBox.value()*100:
-                self.ampSpinBox.setStyleSheet("background-color: green")
-            else:
-                self.ampSpinBox.setStyleSheet("background-color: red")
+            # print(a)
+            self.setDisplay(a)
         except :
             print("\nERROR\nRefreshing GUI-values failed!\n")
+            
+    def ifValLCD(self, value, QObject):
+        if value != QObject.value():
+            QObject.display(value)
+            
+    def setDisplay(self, a):
+        self.ifValLCD(a[2]/100, self.voltageLCD)
+        self.ifValLCD(a[3]/100, self.ampLCD)
+        self.ifValLCD(a[4]/100, self.powerLCD)
+        if self.vInLabel.text() != str(a[5]/100)+" V (V-In)":
+            self.vInLabel.setText(str(a[5]/100)+" V (V-In)")
+        if self.voltageSpinBox.maximum() != round(a[5]/100-1,2):
+            self.voltageSpinBox.setMaximum(round(a[5]/100-1,2))
+        if(a[9]==1):
+            self.powerButton.setStyleSheet("background-color: green")
+        else:
+            self.powerButton.setStyleSheet("background-color: red")
+        if a[0]/100==round(self.voltageSpinBox.value(),2):
+            self.voltageSpinBox.setStyleSheet("background-color: green")
+        else:
+            self.voltageSpinBox.setStyleSheet("background-color: red")
+        if a[1]/100==round(self.ampSpinBox.value(),2):
+            self.ampSpinBox.setStyleSheet("background-color: green")
+        else:
+            self.ampSpinBox.setStyleSheet("background-color: red")
+        if a[6]==1:
+            self.lockedButton.setStyleSheet("background-color: red")
+            self.lockedButton.setText("Locked")
+        else:
+            self.lockedButton.setStyleSheet("background-color: green")
+            self.lockedButton.setText("Unlocked")
+        if a[7]==1:
+            self.protectedLabel.setStyleSheet("background-color: red")
+            self.protectedLabel.setText("Not Protected")
+        else:
+            self.protectedLabel.setStyleSheet("background-color: green")
+            self.protectedLabel.setText("Protected")
+        if a[8]==1:
+            self.cvccLabel.setText("Konst. Strom")
+        else:
+            self.cvccLabel.setText("Konst. Spannung")
             
     def closeEvent(self, event):
         self.closeWindow()
 
     def closeWindow(self):
         print("Closing PyQtDPS")
+        self.power_supply.write_register(0,0)
+        self.power_supply.write_register(1,0)
+        self.power_supply.write_register(9,0)
         global stopMe
         stopMe = True
         self.running = False
-        self.process.kill()
+        if self.processRunning:
+            self.process.kill()
         self.close()
         
+    def initPlotWindow(self):
+        pg.setConfigOption('background', (49, 54, 59))
+        self.plotDialog = QtWidgets.QDialog()
+        layout = QtWidgets.QVBoxLayout(self.plotDialog)
+        self.plotWidget = pg.PlotWidget()
+        ax = self.plotWidget.getAxis('bottom')  # This is the trick
+        # Set grid x and y-axis
+        ax.setGrid(255)
+        ax.setLabel("Zeit [s]")
+        ay = self.plotWidget.getAxis('left')
+        ay.setGrid(255)
+        ay.setLabel("Daten")
+        self.plotWidget.addLegend()
+        self.plotWidget.getPlotItem().setTitle("Spannung/Strom/Leistung")
+        layout.addWidget(self.plotWidget)
+        self.p = [0]*3
+        pen = pg.mkPen(color=(255, 61, 61))
+        self.p[0] = self.plotWidget.plot(
+            x=[0], y=[0], pen=pen, name='Spannung [V]')
+        pen = pg.mkPen(color=(57, 255, 20), )
+        self.p[1] = self.plotWidget.plot(
+            x=[0], y=[0], pen=pen, name='Strom [A]')
+        pen = pg.mkPen(color=(214, 255, 127), style=QtCore.Qt.DotLine)
+        self.p[2] = self.plotWidget.plot(
+            x=[0], y=[0], pen=pen, fillLevel=0, fillBrush=(214, 255, 127, 30), name='Leistung [W]')        
+        self.plotTime = [0]
+        self.plotVolt = [0]
+        self.plotAmp = [0]
+        self.plotPower = [0]
+        self.plotDialog.show()
+        # self.plotter = UpdaterFast(self)
+        # self.plotter.received.connect(self.plotValues)
+        self.plotter = QtCore.QTimer()
+        self.plotter.setInterval(self.config["plot_update_rate"])
+        self.plotter.timeout.connect(self.plotValues)
+        self.plotter.start()
+        
+    def plotValues(self):
+        a=self.power_supply.read_registers(0,11)
+        now = time.time()
+        self.plotTime.append(float(now))
+        self.plotVolt.append(a[2]/100)
+        self.plotAmp.append(a[3]/100)
+        self.plotPower.append(a[4]/100)
+        if float(now)-self.plotTime[0] > self.config["plotLength"]:
+            self.plotTime = self.plotTime[1:]
+            self.plotVolt = self.plotVolt[1:]
+            self.plotAmp = self.plotAmp[1:]
+            self.plotPower = self.plotPower[1:]
+        plotTimeRelative = []
+        for x in self.plotTime:
+            plotTimeRelative.append(x-self.plotTime[len(self.plotTime)-1])
+        self.p[0].setData(x=plotTimeRelative, y=self.plotVolt)
+        self.p[1].setData(x=plotTimeRelative, y=self.plotAmp)
+        self.p[2].setData(x=plotTimeRelative, y=self.plotPower)
+            
 def load_config(path="config.json"):
     with open(path, encoding="UTF-8") as jsonfile:
         config = json.load(jsonfile, encoding="UTF-8")
